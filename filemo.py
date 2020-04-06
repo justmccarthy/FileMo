71ef5d5ebdcd0138469cd65e98b3cf3a3ae53809
Justin McCarthy <mccar085@cougars.csusm.edu>
2020-04-06 15:06:03 -0700

    Basic first page

---
+++ filemo.py
@@ -0,0 +1,83 @@
+import tkinter as tk
+from tkinter import filedialog
+from tkfilebrowser import askopendirnames
+
+class SampleApp(tk.Tk):
+
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.files = []
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (StartPage, CodePage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("StartPage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is the start page")
+        label.pack(side="top", fill="x", pady=10)
+
+        self.selected_list = tk.Listbox(self, height=20, width=50)
+        self.selected_list.pack(padx=15)
+
+        file_button = tk.Button(self, text="Select files",
+                                command=self.openFiles)
+        file_button.pack()
+
+        file_button = tk.Button(self, text="Remove file",
+                                command=self.removeFile)
+        file_button.pack()
+
+        next_button = tk.Button(self, text="Next",
+                                command=lambda: controller.show_frame("CodePage"))
+        next_button.pack()
+
+    def openFiles(self):
+        temp = askopendirnames()
+        self.controller.files = list(temp)
+        for x in self.controller.files:
+            self.selected_list.insert('end', x)
+
+    def removeFile(self):
+        selected = self.selected_list.curselection()
+        value = self.selected_list.get(selected[0])
+        self.selected_list.delete(selected[0])
+        self.controller.files.remove(value)
+
+
+class CodePage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="This is the coding page")
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack()
+
+
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
